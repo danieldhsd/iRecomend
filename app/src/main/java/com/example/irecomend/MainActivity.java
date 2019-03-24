191eed6916104b4a0467660fd0a01c08280ffbcf
DenisWO <o.denis96@gmail.com>
2019-03-23 21:18:14 -0300

    Criando botões para facilitar a comunicação com as outras Activities

--- app/src/main/java/com/example/irecomend/MainActivity.java
+++ app/src/main/java/com/example/irecomend/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.irecomend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = (Button) this.findViewById(R.id.btnLogin);
+        Button btnCadastroCliente = (Button) this.findViewById(R.id.btnCadastroCliente);
+        Button btnCriacaoEvento = (Button) this.findViewById(R.id.btnCriacaoEvento);
+        Button btnCarregamentoEvento = (Button) this.findViewById(R.id.btnCarregamentoEvento);
+//
+//        btnLogin.setOnClickListener((View) this);
+//        btnCadastroCliente.setOnClickListener((View.OnClickListener) this);
+//        btnCriacaoEvento.setOnClickListener((View.OnClickListener) this);
+//        btnCarregamentoEvento.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View view){
+        if(view.getId() == R.id.btnCadastroCliente){
+            Toast.makeText(this, "Cadastro de cliente", Toast.LENGTH_LONG);
+        }
+        else if(view.getId() == R.id.btnLogin){
+            Toast.makeText(this, "Login", Toast.LENGTH_LONG);
+        }
+        else if(view.getId() == R.id.btnCriacaoEvento){
+            Toast.makeText(this, "Criacao de Evento", Toast.LENGTH_LONG);
+        }
+        else if(view.getId() == R.id.btnCarregamentoEvento){
+            Toast.makeText(this, "Carregamento de evento", Toast.LENGTH_LONG);
+        }
     }
 }
