af04448cabb0b18a6ef576260f5ccbd345a03b61
Daniel Dutra <danieldhsd@gmail.com>
2019-03-27 15:46:00 -0300

    Começando a tela de cadastro de evento.

--- app/src/main/java/com/example/irecomend/TelaCadastroEvento.java
+++ app/src/main/java/com/example/irecomend/TelaCadastroEvento.java
@@ -2,12 +2,25 @@ package com.example.irecomend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class TelaCadastroEvento extends AppCompatActivity {
+
+    EditText nomeEvento;
+    EditText data;
+    EditText horario;
+    EditText cep;
+    EditText rua;
+    EditText numero;
+    EditText bairro;
+    EditText cidade;
+    EditText pais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro_evento);
+
+
     }
 }
