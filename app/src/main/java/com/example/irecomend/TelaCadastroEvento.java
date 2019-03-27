359d630ac9f8843aa4994d2c31614c0c7b7c7306
danieldhsd <danieldhsd@gmail.com>
2019-03-27 15:54:45 -0300

    Começando a tela de cadastro de evento.

--- app/src/main/java/com/example/irecomend/TelaCadastroEvento.java
+++ app/src/main/java/com/example/irecomend/TelaCadastroEvento.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.example.irecomend.connection.Conexao;
+
 public class TelaCadastroEvento extends AppCompatActivity {
 
     EditText nomeEvento;
@@ -16,11 +18,23 @@ public class TelaCadastroEvento extends AppCompatActivity {
     EditText cidade;
     EditText pais;
 
+    Conexao db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro_evento);
 
+        db = new Conexao( this);
 
+        nomeEvento = (EditText)findViewById(R.id.nomeEvento);
+        data = (EditText)findViewById(R.id.DataEvento);
+        horario = (EditText)findViewById(R.id.Horario);
+        cep = (EditText)findViewById(R.id.CEP);
+        rua = (EditText)findViewById(R.id.CEP);
+        numero = (EditText)findViewById(R.id.Numero);
+        bairro = (EditText)findViewById(R.id.Bairro);
+        cidade = (EditText)findViewById(R.id.Cidade);
+        pais = (EditText)findViewById(R.id.Pais);
     }
 }
