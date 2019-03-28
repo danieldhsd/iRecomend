4994da1cc197c8cab32d8646f98f44ad2cb6bb18
Tamires <tamiresvilaca@hotmail.com>
2019-03-27 21:18:16 -0300

    Alterações cadastro cliente

--- app/src/main/java/com/example/irecomend/TelaCadastroCliente.java
+++ app/src/main/java/com/example/irecomend/TelaCadastroCliente.java
@@ -5,18 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-import android.widget.Toolbar;
+
 
 import com.example.irecomend.connection.Conexao;
+import com.example.irecomend.model.bean.Cliente;
+import com.example.irecomend.model.dao.ClienteDAO;
 
 public class TelaCadastroCliente extends AppCompatActivity {
+
+    private Cliente cliente;
+    private ClienteDAO clienteDAO;
+
 
     EditText nomeEditText;
     EditText sobrenomeEditText;
@@ -30,83 +31,85 @@ public class TelaCadastroCliente extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro_cliente);
 
-        /*
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
+        this.nomeEditText = this.findViewById(R.id.nomeEditText);
+        this.sobrenomeEditText = this.findViewById(R.id.sobrenomeEditText);
+        this.emailEditText = this.findViewById(R.id.emailEditText);
+        this.senhaEditText = this.findViewById(R.id.senhaEditText);
+        this.confirmarEdittext = this.findViewById(R.id.confirmarEdittext);
 
-        db = new Conexao(this);
-        nomeEditText = (EditText)findViewById(R.id.nomeEditText);
-        sobrenomeEditText = (EditText)findViewById(R.id.sobrenomeEditText);
-        emailEditText = (EditText)findViewById(R.id.emailEditText);
-        senhaEditText = (EditText)findViewById(R.id.senhaEditText);
-        confirmarEdittext = (EditText)findViewById(R.id.confirmarEdittext);
+        validaCampos();
 
-        public void validaCampos()
-        {
-            boolean res = false;
+        clienteDAO = new ClienteDAO(this);
+        cliente = new Cliente(this.nomeEditText.getText().toString(),this.sobrenomeEditText.getText().toString(),this.emailEditText.getText().toString(),this.senhaEditText.getText().toString());
+        clienteDAO.inserirCliente(cliente);
 
-            String nome=nomeEditText.getText().toString();
-            String sobrenome=sobrenomeEditText.getText().toString();
-            String email=emailEditText.getText().toString();
-            String senha=emailEditText.getText().toString();
-            String confirmacao=emailEditText.getText().toString();
-            /*SE O CAMPO ESTIVER VAZIO VOLTA FOCO PRO PRÓPRIO CAMPO*/
-            if(isCampoVazio(nome))
-            {
-                nomeEditText.requestFocus();
-                res = true;
-            }
-            else if(isCampoVazio(sobrenome))
-            {
-                sobrenomeEditText.requestFocus();
-                res = true;
-            }
-            else if(isCampoVazio(email) || !isEmailVazio(email))
-            {
-                emailEditText.requestFocus();
-                res = true;
-            }
-            else if(isCampoVazio(senha))
-            {
-                senhaEditText.requestFocus();
-                res = true;
-            }
-            else if(isCampoVazio(confirmacao)||!isIguais(senha,confirmacao))
-            {
-                confirmarEdittext.requestFocus();
-                res = true;
-            }
+    }
+    public void validaCampos()
+    {
+        boolean res = false;
 
-            if(res)
-            {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(this);
-                dlg.setTitle("Aviso");
-                dlg.setMessage("Há campos inválidos ou em branco!");
-                dlg.setNeutralButton("OK",null);
-                dlg.show();
-            }
-        }
-        /*VERIFICA SE O CAMPO ESTÁ VAZIO*/
-        private boolean isCampoVazio(String valor)
+        String nome=nomeEditText.getText().toString();
+        String sobrenome=sobrenomeEditText.getText().toString();
+        String email=emailEditText.getText().toString();
+        String senha=emailEditText.getText().toString();
+        String confirmacao=emailEditText.getText().toString();
+        /*SE O CAMPO ESTIVER VAZIO VOLTA FOCO PRO PRÓPRIO CAMPO*/
+        if(isCampoVazio(nome))
         {
-            boolean resultado = (TextUtils.isEmpty(valor)||valor.trim().isEmpty());
-            return resultado;
+            nomeEditText.requestFocus();
+            res = true;
         }
-        /*VERIFICA SE EMAIL É VÁLIDO*/
-        private boolean isEmailValido(String email)
+        else if(isCampoVazio(sobrenome))
         {
-            boolean resultado = (!isCampoVazio(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
-            return resultado;
+            sobrenomeEditText.requestFocus();
+            res = true;
         }
-        /*VERIFICA SE SENHAS SAO IGUAIS*/
-        private boolean isIguais(String senha, String confirmacao)
+        else if((isCampoVazio(email)) || (!isEmailValido(email)))
         {
-            if(senha == confirmacao)
-            {
-                return true;
-            }
-            return false;
+            emailEditText.requestFocus();
+            res = true;
         }
+        else if(isCampoVazio(senha))
+        {
+            senhaEditText.requestFocus();
+            res = true;
+        }
+        else if(isCampoVazio(confirmacao)||!isIguais(senha,confirmacao))
+        {
+            confirmarEdittext.requestFocus();
+            res = true;
+        }
+
+        if(res)
+        {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle("Aviso");
+            dlg.setMessage("Há campos inválidos ou em branco!");
+            dlg.setNeutralButton("OK",null);
+            dlg.show();
+        }
+    }
+    /*VERIFICA SE O CAMPO ESTÁ VAZIO*/
+    private boolean isCampoVazio(String valor)
+    {
+        boolean resultado = (TextUtils.isEmpty(valor)||valor.trim().isEmpty());
+        return resultado;
+    }
+    /*VERIFICA SE EMAIL É VÁLIDO*/
+    private boolean isEmailValido(String email)
+    {
+        boolean resultado = (!isCampoVazio(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        return resultado;
+    }
+    /*VERIFICA SE SENHAS SAO IGUAIS*/
+    private boolean isIguais(String senha, String confirmacao)
+    {
+        if(senha == confirmacao)
+        {
+            return true;
+        }
+        return false;
+    }
 
 
         /*cadastrarBotao.setOnClickListener(new View.OnClickListener() {
@@ -127,35 +130,32 @@ public class TelaCadastroCliente extends AppCompatActivity {
 
             }*/
 
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cadastro_cliente);
+        return super.onCreateOptionsMenu(menu);
+    }*/
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        switch (id)
         {
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.menu_cadastro_cliente);
-            return super.onCreateOptionsMenu(menu);
-        });
+            case R.id.action_cadastrar:
+                validaCampos();
+                //Toast.makeText(this,"Usuário Cadastrado", Toast.LENGTH_SHORT.show());
+                break;
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item)
-        {
-            int id = item.getItemId();
-            switch (id)
-            {
-                case R.id.action_cadastrar:
-                    validaCampos();
-                        //Toast.makeText(this,"Usuário Cadastrado", Toast.LENGTH_SHORT.show());
-                    break;
+            case R.id.action_cancelar:
 
-                case R.id.action_cancelar:
-
-                    //Toast.makeText(this,"Cadastro Cancelado", Toast.LENGTH_SHORT.show());
-                    finish();
-                    break;
-            }
-            return super.onOptionsItemSelected(item);
+                //Toast.makeText(this,"Cadastro Cancelado", Toast.LENGTH_SHORT.show());
+                finish();
+                break;
         }
-
-
+        return super.onOptionsItemSelected(item);
     }
 }
